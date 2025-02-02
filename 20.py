df3aec3107b1bb5f45f77aaef79ffdcf81d3c4cb
Abhinandan Kumar <kumarabhinandan1307@gmail.com>
2025-02-02 10:14:54 +0530

    Add files via upload

---
+++ 20.py
@@ -0,0 +1,9 @@
+Python 3.14.0a4 (tags/v3.14.0a4:f26daa9, Jan 14 2025, 13:27:29) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n = input()
+... n1 = n[:: -1]
+... if(int(n)==int(n1)):
+...   print("palindrom")
+... else:
+...   print("not palindrom")
+... 
